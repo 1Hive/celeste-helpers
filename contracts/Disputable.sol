1181c77e84c009b25ac54e1c48af0a126e935762
MrClottom <philippe@dumo.net>
2021-06-14 00:02:31 +0200

    Move manifest interface to interfaces folder

--- contracts/Disputable.sol
+++ contracts/Disputable.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IArbitrator.sol";
-import "./manifest/IArbitratorManifest.sol";
+import "./interfaces/IArbitratorManifest.sol";
 
 abstract contract Disputable {
     using SafeERC20 for IERC20;
