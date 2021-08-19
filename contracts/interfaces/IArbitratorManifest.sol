d445e2629bd308577bccb709f1ee690eac66af5a
MrClottom <philippe@dumo.net>
2021-08-20 01:36:45 +0200

    Prevent rep conflict and allow rep recusing

--- contracts/interfaces/IArbitratorManifest.sol
+++ contracts/interfaces/IArbitratorManifest.sol
@@ -7,24 +7,24 @@ interface IArbitratorManifest {
         address indexed defendant,
         address indexed challenger
     );
-    event RepStateChanged(
+    event RepStateSet(
         address indexed account,
         address indexed rep,
         bool isActive
     );
+    event RecusalSet(
+        address indexed rep,
+        address indexed client,
+        bool recused
+    );
 
-    function setPartiesOf(
-        uint256 _disputeId,
-        address _defendant,
-        address _challenger
-    ) external;
-    function setRepStatus(
-        address _rep,
-        bool _isActive
-    ) external;
+    function setPartiesOf(uint256 _disputeId, address _defendant, address _challenger) external;
+    function setRepStatus(address _rep, bool _isActive) external;
+    function setRecused(address _client, bool _recuseSelf) external;
     function isRepOf(address _account, address _rep) external view returns (bool isRep);
     function defendantOf(uint256 _disputeId) external view returns (address defendant);
     function challengerOf(uint256 _disputeId) external view returns (address challenger);
+    function recusedFor(address _rep, address _client) external view returns (bool isRecused);
     function canSubmitEvidenceFor(address _submitter, uint256 _disputeId)
         external view returns (bool canSubmit, address submittingFor);
 }
--- contracts/manifest/ArbitratorManifestCore.sol
+++ contracts/manifest/ArbitratorManifestCore.sol
@@ -5,6 +5,7 @@ import "../interfaces/IArbitratorManifest.sol";
 
 abstract contract ArbitratorManifestCore is IArbitratorManifest {
     mapping(address => mapping(address => bool)) public override isRepOf;
+    mapping(address => mapping(address => bool)) public override recusedFor;
     mapping(uint256 => address) public override defendantOf;
     mapping(uint256 => address) public override challengerOf;
 
@@ -22,23 +23,39 @@ abstract contract ArbitratorManifestCore is IArbitratorManifest {
     }
 
     function setRepStatus(address _rep, bool _isActive) external override {
-        require(isRepOf[msg.sender][_rep] != _isActive, "ArbitratorManifest: already set");
-        isRepOf[msg.sender][_rep] = _isActive;
-        emit RepStateChanged(msg.sender, _rep, _isActive);
+        _setRepStatus(msg.sender, _rep, _isActive);
+    }
+
+    function setRecused(address _client, bool _recuseSelf) external override {
+        if (_recuseSelf) {
+            _setRepStatus(_client, msg.sender, false);
+        }
+        recusedFor[msg.sender][_client] = _recuseSelf;
+        emit RecusalSet(msg.sender, _client, _recuseSelf);
     }
 
     function canSubmitEvidenceFor(address _submitter, uint256 _disputeId)
         public view override returns (bool, address)
     {
         address defendant = defendantOf[_disputeId];
-        if (defendant == _submitter || isRepOf[defendant][_submitter]) {
+        if (defendant == _submitter) {
             return (true, defendant);
         }
         address challenger = challengerOf[_disputeId];
+        if (isRepOf[defendant][_submitter]) {
+            require(!isRepOf[challenger][_submitter], "ArbitratorManifest: rep conflict");
+            return (true, defendant);
+        }
         if (challenger == _submitter || isRepOf[challenger][_submitter]) {
             return (true, challenger);
         }
         return (false, address(0));
+    }
+
+    function _setRepStatus(address _client, address _rep, bool _isActive) internal {
+        require(!_isActive || !recusedFor[_rep][_client], "ArbitratorManifest: rep recused");
+        isRepOf[msg.sender][_rep] = _isActive;
+        emit RepStateSet(msg.sender, _rep, _isActive);
     }
 
     function _getSubjectOf(uint256 _disputeId)
